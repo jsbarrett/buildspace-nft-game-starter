f46faade16e989a48887084085b1bf5c5e3f84ab
Jacob Barrett <jsbarrett88@gmail.com>
2021-10-29 09:57:09 -0500

    Updates from buildspace "lessons"

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x7c7a176D911D20c46d7861E5bbf3bd64b2C8C1e9';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
